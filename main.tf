46ffd9a4c4d8c283025194ffd00f220d391cbe30
lucasrochex <lucasrochex@gmail.com>
2021-08-23 21:10:04 -0200

    emr 7

--- main.tf
+++ main.tf
@@ -33,18 +33,6 @@ resource "aws_emr_cluster" "cluster" {
   release_label = "emr-6.1.0"
   applications  = ["Spark","Hive","Pig","Hue","Livy","JupyterHub"]
 
-  additional_info = <<EOF
-{
-  "instanceAwsClientConfiguration": {
-    "proxyPort": 8099,
-    "proxyHost": "myproxy.example.com"
-  }
-}
-EOF
-
-  termination_protection            = false
-  keep_job_flow_alive_when_no_steps = true
-
   ec2_attributes {
     subnet_id                         = aws_subnet.main.id
     emr_managed_master_security_group = aws_security_group.allow_all.id
@@ -57,57 +45,17 @@ EOF
   }
 
   core_instance_group {
-    instance_type  = "m5.xlarge"
     instance_count = 2
-
-    ebs_config {
-      size                 = "40"
-      type                 = "gp2"
-      volumes_per_instance = 1
-    }
-
-    bid_price = "0.04"
-
-    autoscaling_policy = <<EOF
-{
-"Constraints": {
-  "MinCapacity": 1,
-  "MaxCapacity": 2
-},
-"Rules": [
-  {
-    "Name": "ScaleOutMemoryPercentage",
-    "Description": "Scale out if YARNMemoryAvailablePercentage is less than 15",
-    "Action": {
-      "SimpleScalingPolicyConfiguration": {
-        "AdjustmentType": "CHANGE_IN_CAPACITY",
-        "ScalingAdjustment": 1,
-        "CoolDown": 300
-      }
-    },
-    "Trigger": {
-      "CloudWatchAlarmDefinition": {
-        "ComparisonOperator": "LESS_THAN",
-        "EvaluationPeriods": 1,
-        "MetricName": "YARNMemoryAvailablePercentage",
-        "Namespace": "AWS/ElasticMapReduce",
-        "Period": 300,
-        "Statistic": "AVERAGE",
-        "Threshold": 15.0,
-        "Unit": "PERCENT"
-      }
-    }
-  }
-]
-}
-EOF
+    instance_type  = "m5.xlarge"
   }
 
-  ebs_root_volume_size = 100
+  bid_price = "0.04"
 
   tags = {
-    IES   = "IGTI",
-    CURSO = "EDC"
+    role     = "rolename"
+    dns_zone = "env_zone"
+    env      = "env"
+    name     = "name-env"
   }
 
   bootstrap_action {
@@ -144,25 +92,10 @@ EOF
     }
   ]
 EOF
+
   service_role = aws_iam_role.iam_emr_service_role.arn
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "168.31.0.0/20"
-
-  tags = {
-    name = "emr_test"
-  }
-}
-resource "aws_vpc" "main" {
-  cidr_block           = "168.31.0.0/16"
-  enable_dns_hostnames = true
-
-  tags = {
-    name = "emr_test"
-  }
-}
 resource "aws_security_group" "allow_access" {
   name        = "allow_access"
   description = "Allow inbound traffic"
@@ -196,11 +129,140 @@ resource "aws_security_group" "allow_access" {
   }
 }
 
-resource "aws_iam_instance_profile" "emr_profile" {
-  name = "emr_profile"
-  role = aws_iam_role.iam_emr_profile_role.name
+resource "aws_vpc" "main" {
+  cidr_block           = "168.31.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    name = "emr_test"
+  }
 }
 
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "168.31.0.0/20"
+
+  tags = {
+    name = "emr_test"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_main_route_table_association" "a" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.r.id
+}
+
+###
+
+# IAM Role setups
+
+###
+
+# IAM role for EMR Service
+resource "aws_iam_role" "iam_emr_service_role" {
+  name = "iam_emr_service_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "elasticmapreduce.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "iam_emr_service_policy" {
+  name = "iam_emr_service_policy"
+  role = aws_iam_role.iam_emr_service_role.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [{
+        "Effect": "Allow",
+        "Resource": "*",
+        "Action": [
+            "ec2:AuthorizeSecurityGroupEgress",
+            "ec2:AuthorizeSecurityGroupIngress",
+            "ec2:CancelSpotInstanceRequests",
+            "ec2:CreateNetworkInterface",
+            "ec2:CreateSecurityGroup",
+            "ec2:CreateTags",
+            "ec2:DeleteNetworkInterface",
+            "ec2:DeleteSecurityGroup",
+            "ec2:DeleteTags",
+            "ec2:DescribeAvailabilityZones",
+            "ec2:DescribeAccountAttributes",
+            "ec2:DescribeDhcpOptions",
+            "ec2:DescribeInstanceStatus",
+            "ec2:DescribeInstances",
+            "ec2:DescribeKeyPairs",
+            "ec2:DescribeNetworkAcls",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:DescribePrefixLists",
+            "ec2:DescribeRouteTables",
+            "ec2:DescribeSecurityGroups",
+            "ec2:DescribeSpotInstanceRequests",
+            "ec2:DescribeSpotPriceHistory",
+            "ec2:DescribeSubnets",
+            "ec2:DescribeVpcAttribute",
+            "ec2:DescribeVpcEndpoints",
+            "ec2:DescribeVpcEndpointServices",
+            "ec2:DescribeVpcs",
+            "ec2:DetachNetworkInterface",
+            "ec2:ModifyImageAttribute",
+            "ec2:ModifyInstanceAttribute",
+            "ec2:RequestSpotInstances",
+            "ec2:RevokeSecurityGroupEgress",
+            "ec2:RunInstances",
+            "ec2:TerminateInstances",
+            "ec2:DeleteVolume",
+            "ec2:DescribeVolumeStatus",
+            "ec2:DescribeVolumes",
+            "ec2:DetachVolume",
+            "iam:GetRole",
+            "iam:GetRolePolicy",
+            "iam:ListInstanceProfiles",
+            "iam:ListRolePolicies",
+            "iam:PassRole",
+            "s3:CreateBucket",
+            "s3:Get*",
+            "s3:List*",
+            "sdb:BatchPutAttributes",
+            "sdb:Select",
+            "sqs:CreateQueue",
+            "sqs:Delete*",
+            "sqs:GetQueue*",
+            "sqs:PurgeQueue",
+            "sqs:ReceiveMessage"
+        ]
+    }]
+}
+EOF
+}
+
+# IAM Role for EC2 Instance Profile
 resource "aws_iam_role" "iam_emr_profile_role" {
   name = "iam_emr_profile_role"
 
@@ -220,22 +282,47 @@ resource "aws_iam_role" "iam_emr_profile_role" {
 }
 EOF
 }
-resource "aws_iam_role" "iam_emr_service_role" {
-  name = "iam_emr_service_role"
 
-  assume_role_policy = <<EOF
+resource "aws_iam_instance_profile" "emr_profile" {
+  name = "emr_profile"
+  role = aws_iam_role.iam_emr_profile_role.name
+}
+
+resource "aws_iam_role_policy" "iam_emr_profile_policy" {
+  name = "iam_emr_profile_policy"
+  role = aws_iam_role.iam_emr_profile_role.id
+
+  policy = <<EOF
 {
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "elasticmapreduce.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [{
+        "Effect": "Allow",
+        "Resource": "*",
+        "Action": [
+            "cloudwatch:*",
+            "dynamodb:*",
+            "ec2:Describe*",
+            "elasticmapreduce:Describe*",
+            "elasticmapreduce:ListBootstrapActions",
+            "elasticmapreduce:ListClusters",
+            "elasticmapreduce:ListInstanceGroups",
+            "elasticmapreduce:ListInstances",
+            "elasticmapreduce:ListSteps",
+            "kinesis:CreateStream",
+            "kinesis:DeleteStream",
+            "kinesis:DescribeStream",
+            "kinesis:GetRecords",
+            "kinesis:GetShardIterator",
+            "kinesis:MergeShards",
+            "kinesis:PutRecord",
+            "kinesis:SplitShard",
+            "rds:Describe*",
+            "s3:*",
+            "sdb:*",
+            "sns:*",
+            "sqs:*"
+        ]
+    }]
 }
 EOF
 }
