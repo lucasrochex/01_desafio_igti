7c916247805bf56b9ff46511441a94ff95211d35
lucasrochex <lucasrochex@gmail.com>
2021-08-23 21:43:42 -0200

    emr 12

--- main.tf
+++ main.tf
@@ -154,7 +154,7 @@ resource "aws_route_table" "r" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = ["0.0.0.0/0"]
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 }
